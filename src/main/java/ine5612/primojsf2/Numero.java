4d89b30a5280071f06fa7ec6f1131031cdea90ae
Martin <martin.vigil@ufsc.br>
2017-04-17 14:56:01 -0300

    versao incompleta para alunos

---
+++ src/main/java/ine5612/primojsf2/Numero.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ine5612.primojsf2;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+/**
+ *
+ * @author martin
+ */
+@ManagedBean(name = "???")
+@RequestScoped
+public class Numero {
+
+    //implementar
+    
+}
